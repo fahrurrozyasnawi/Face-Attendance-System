1499dd64f64b5404d5d90780e567025984ca4797
fahrurrozyasnawi <fahrurrozyasnawi@gmail.com>
2021-05-31 14:53:56 +0800

    List

---
+++ frontend/src/components/mahasiswa/List/Angkatan.js
@@ -0,0 +1,18 @@
+const angkatan = [
+  {
+    value: 2017,
+    label: '2017'
+  },
+  {
+    value: 2018,
+    label: '2018'
+  },
+  {
+    value: 2019,
+    label: '2019'
+  },
+  {
+    value: 2020,
+    label: '2020'
+  }
+]
